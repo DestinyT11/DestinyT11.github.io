48aa6d3bc2f6656380e782086b1f7f69d7673872
Destiny <55287504+DestinyT11@users.noreply.github.com>
2020-01-15 11:10:18 -0500

    Update myScripts.js

--- tests/myScripts.js
+++ tests/myScripts.js
@@ -1,1 +1,11 @@
 
+for(var i = 0; i < 10; i++ ){
+  var ans = Math.random(i);
+  document.write("Loop: " + ans + "<br/>");
+}
+
+document.write("Part B: <br/>");
+for(var i = 0; i < 20; i++ ){
+  var ans = Math.sqrt(i);
+  document.write("Loop:" + ans + "<br/>")
+}
