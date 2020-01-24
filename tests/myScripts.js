7b1fd72c0895e79adbea43388658bfaf5b386bf5
Destiny <55287504+DestinyT11@users.noreply.github.com>
2020-01-24 11:24:37 -0500

    Update myScripts.js

--- tests/myScripts.js
+++ tests/myScripts.js
@@ -9,15 +9,27 @@ for(var i = 0; i < 20; i++ ){
   var ans = Math.sqrt(i);
   document.write("Loop:" + ans + "<br/>")
 }
-
         
-        
+          
 function myLoop(){
 var end = document.getElementById("end").value;
-
-  var start = document.getElementById("start").value;
-
-for(var i = 0; i <= end; i++)
+var start = document.getElementById("start").value;
+var jump = document.getElementById("jump").value;
+  
+    start = Number(start);
+  end = Number(end);
+  jump = Number(jump);
+  
+ console.log(start) 
+console.log(typeof start);
+console.log(typeof end);
+console.log(typeof jump);
+  
+  start = Number(start);
+  end = Number(end);
+  jump = Number(jump);
+  
+for(var i = 0; i <= end; i+=jump)
 {
   document.write('<p>' + i);
 }
